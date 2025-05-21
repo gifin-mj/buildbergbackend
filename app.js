05e336b49d9487a687e1662967fd1bd673a7a780
gifin-mj <gifinmj@gmail.com>
2025-05-21 20:14:16 +0530

    morgan

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
+var logger = require('morgan');
 const authRoutes = require('./routes/auth');
 const galleryRoutes = require('./routes/gallery');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(logger('dev'));
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
